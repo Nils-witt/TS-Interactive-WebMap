876822ea7e30e2fc6d5c0719cd14cbe196d72756
Nils Witt <nils.witt@nils-witt.de>
2025-10-15 23:01:50 +0200

    Add CacheManager and integrate cache clearing functionality in SettingsControl

--- src/controls/SettingsControl.ts
+++ src/controls/SettingsControl.ts
@@ -4,6 +4,8 @@ import {faGear} from "@fortawesome/free-solid-svg-icons/faGear";
 import {icon} from "@fortawesome/fontawesome-svg-core";
 import {useControl} from "@vis.gl/react-maplibre";
 import {ApiProvider} from "../dataProviders/ApiProvider.ts";
+import {CacheManager} from "../dataProviders/CacheManager";
+import {faXmark} from "@fortawesome/free-solid-svg-icons/faXmark";
 
 type ReactSettingsControlProps = {
     position: ControlPosition;
@@ -33,45 +35,58 @@ export class SettingsControl implements IControl {
         this.container.classList.add("p-[5px]");
 
 
-
         this.iconSpan = document.createElement("span");
         this.iconSpan.innerHTML = icon(faGear, {
             transform: {}
         }).html[0];
         this.container.appendChild(this.iconSpan)
 
-        this.container.addEventListener("click", () => {
-            console.log("Settings clicked");
-            if(!this.isShown){
-                this.showSettings();
-            }
+        this.iconSpan.addEventListener("click", () => {
+            this.toggleOpen();
         })
 
         this.setUpUI();
     }
 
 
-    private setUpUI(){
+    private setUpUI() {
         this.settingsContainer = DOM.create("div");
         this.container.appendChild(this.settingsContainer);
-        this.container.classList.add( "bg-white")
+        this.container.classList.add("bg-white");
 
         this.settingsContainer.classList.add("grid", "hidden");
 
         const logoutBtn = DOM.create("button");
-        logoutBtn.classList.add("w-[5em]", "bg-red-500");
+        logoutBtn.classList.add("w-[5em]", "h-[2.5em]", "bg-red-500");
         logoutBtn.textContent = "Logout";
         this.settingsContainer.appendChild(logoutBtn);
         logoutBtn.addEventListener("click", () => {
-            console.log("Logout clicked");
-            ApiProvider.getInstance().logout();
-        })
+            if (confirm("Are you sure you want to logout?")) {
+                ApiProvider.getInstance().logout();
+            }
+        });
+
+        const clearCacheBtn = DOM.create("button");
+        clearCacheBtn.classList.add("w-[8em]", "h-[2.5em]", "mt-[10px]", "bg-orange-500");
+        clearCacheBtn.textContent = "Clear Cache";
+        this.settingsContainer.appendChild(clearCacheBtn);
+        clearCacheBtn.addEventListener("click", () => {
+            if (confirm("Are you sure you want to clear the cache?")) {
+                CacheManager.clearMapData();
+            }
+        });
     }
 
-    showSettings() {
-        this.isShown = true;
-        this.settingsContainer.classList.remove("hidden");
-        this.iconSpan.classList.add("hidden");
+    toggleOpen() {
+        if (this.isShown) {
+            this.settingsContainer.classList.add("hidden");
+            this.iconSpan.innerHTML = icon(faGear).html[0];
+        } else {
+            this.settingsContainer.classList.remove("hidden");
+            this.iconSpan.innerHTML = icon(faXmark).html[0];
+        }
+
+        this.isShown = !this.isShown;
     }
 
     onRemove(): void {
