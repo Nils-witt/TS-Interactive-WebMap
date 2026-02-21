f746e7d2f23365025457e8feb9095494220ac93b
Nils Witt <nils.witt@nils-witt.de>
2026-02-22 00:11:52 +0100

    Add settings for excluding status "6" units and hiding units after position update

--- src/enitities/MapConfig.ts
+++ src/enitities/MapConfig.ts
@@ -5,8 +5,9 @@ export class MapConfig {
 
 
     private unitIconSize = parseInt(localStorage.getItem('unit_icon_size') || '50');
-    private excludeStatuses = [6];
+    private excludeStatuses = JSON.parse(localStorage.getItem('exclude_statuses') || '[6]') as number[];
     private provider = new LocalStorageProvider();
+    private hideUnitsAfterPositionUpdate = parseInt(localStorage.getItem('hide_units_after_position_update') || '21600'); // 6 hours in seconds
 
     public getUnitIconSize(): number {
         return this.unitIconSize;
@@ -22,6 +23,16 @@ export class MapConfig {
     }
 
     public setExcludeStatuses(excludeStatuses: number[]): void {
+        void this.provider.setItem('exclude_statuses', JSON.stringify(excludeStatuses));
         this.excludeStatuses = excludeStatuses;
+    }
+
+    public getHideUnitsAfterPositionUpdate(): number {
+        return this.hideUnitsAfterPositionUpdate;
+    }
+
+    public setHideUnitsAfterPositionUpdate(seconds: number): void {
+        void this.provider.setItem('hide_units_after_position_update', seconds.toString());
+        this.hideUnitsAfterPositionUpdate = seconds;
     }
 }
